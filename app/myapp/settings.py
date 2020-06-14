6a0b44dc53b79c7a2f8a5a3465156ce9f6ac9ff3
高明児玉 <sipslqmn921@gmail.com>
2020-06-14 21:32:38 +0900

    db setuped

--- app/myapp/settings.py
+++ app/myapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'app',
+        'USER': 'dev',
+        'PASSWORD': 'dev',
+        'HOST': 'db',
+        'PORT': '',
     }
 }
 
